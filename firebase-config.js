596e3df11147e96b430a4f112eb0605d80ec40db
kamalthakor22 <kamalt@rocketmail.com>
2025-09-26 22:18:57 -0400

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBA4r1N4L_GwTrVrKku3lFYuj1gZIy-45w",
   authDomain: "ajaxairporttransfer.firebaseapp.com",
@@ -7,5 +13,5 @@ const firebaseConfig = {
   appId: "1:664561343575:web:3132b18dfa6ba41118d712"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
